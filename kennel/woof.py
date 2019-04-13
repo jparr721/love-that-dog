027881c9fe0e0c9b7fc595bf2e537cf9574ecfc5
Jarred Parr <jparr721@gmail.com>
2019-04-12 20:17:49 -0400

    woof woof woof

--- kennel/woof.py
+++ kennel/woof.py
@@ -3,30 +3,32 @@
 import keras
 import glob
 import os
-from sklearn.model_selection import train_test_split
+import sys
 import numpy as np
-import pandas as pd
+from PIL import Image
+from idiot import Doofus
+from sklearn.model_selection import train_test_split
 
 
 def build_model(shape: tuple)->keras.models.Sequential:
     return keras.models.Sequential([
         keras.layers.Conv2D(
-            shape.x,
+            shape[0],
             (3, 3),
             padding='same',
             activation='relu',
-            input_shape=(shape.x, shape.y, 3)
+            input_shape=(shape[0], shape[1], shape[2])
         ),
         keras.layers.Dropout(0.3),
         keras.layers.Conv2D(
-            shape.x,
+            shape[0],
             (3, 3),
             padding='same',
             activation='relu'
         ),
         keras.layers.MaxPooling2D(pool_size=(2, 2)),
         keras.layers.Conv2D(
-            shape.x * 2,
+            shape[0] * 2,
             (3, 3),
             padding='same',
             activation='relu'
@@ -74,6 +76,43 @@ def train_model(model: keras.models.Sequential,
 
     loss, acc = model.evaluate(X_test, y_test)
 
-    model.save_weights('showzer.h5')
+    json = model.to_json()
+    with open('woof.json', 'w') as json_file:
+        json_file.write(json)
+    model.save_weights('woof.h5')
 
     print(f'Loss: {loss}, Accuracy: {acc}')
+
+
+def do_the_thing_bro(image: list)->str:
+    json = open('woof.json', 'r')
+    loaded_json = json.read()
+    json.close()
+    the_model = keras.models.model_from_json(loaded_json)
+    the_model.load_weights('woof.h5')
+
+    the_model.compile(optimizer='adam',
+                      loss='mean_squared_error',
+                      metrics=['accuracy'])
+    dimx = 400
+    dimy = 500
+    img = Image.open(image)
+    img.resize((dimx, dimy), Image.ANTIALIAS)
+    the_img = np.array(img)
+
+    return the_model.predict_classes(the_img)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('Error bro, we need more than that')
+
+    opt = sys.argv[1]
+    if not opt.startswith('--'):
+        raise Doofus('Idiot, your flags are wrong')
+
+    if opt == '--train':
+        train_model(build_model(400, 500, 3), './Images', './Annotation')
+
+    if opt == '--go':
+        do_the_thing_bro(sys.argv[2])
