42068587b382aca486753669a3bd70f1381d912e
Jarred Parr <jparr721@gmail.com>
2019-04-14 17:09:33 -0400

    fixed the images and annotations

--- kennel/woof.py
+++ kennel/woof.py
@@ -4,10 +4,16 @@ import keras
 import glob
 import os
 import sys
+import random
 import numpy as np
+import xml.etree.ElementTree as ET
 from PIL import Image
-from idiot import Doofus
 from sklearn.model_selection import train_test_split
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+
+class Doofus(Exception):
+    pass
 
 
 def build_model(shape: tuple)->keras.models.Sequential:
@@ -36,7 +42,7 @@ def build_model(shape: tuple)->keras.models.Sequential:
         keras.layers.MaxPooling2D(pool_size=(2, 2)),
         keras.layers.Flatten(),
         keras.layers.Dense(
-            200000,
+            2500,
             activation='relu'
         ),
         keras.layers.Dropout(0.3),
@@ -57,6 +63,47 @@ def flatten(ls: list):
     return [item for subitem in ls for item in subitem]
 
 
+def fix_images(image_path: str):
+    directories = os.listdir(image_path)
+    images = []
+    output_dir = './img_fixed'
+
+    for d in directories:
+        all_img = glob.glob(image_path + '/' + d + '/*.jpg')
+        images.append(all_img)
+
+    images = flatten(images)
+
+    dimx = 50
+    dimy = 50
+    for i in range(len(images)):
+        print('Fixing image: {}'.format(images[i]))
+        img = Image.open(images[i])
+        img.resize((dimx, dimy), Image.ANTIALIAS)
+        print('Saving...')
+        img.save(output_dir + '/' + 'image_' + str(i) + '.png')
+
+    print('done')
+
+
+def fix_annotations(annotation_path: str):
+    directories = os.listdir(annotation_path)
+    annotations = []
+    for d in directories:
+        anno = d.split('-')[1].capitalize()
+        if '_' in anno:
+            anno.replace('_', ' ')
+            strings = anno.split(' ')
+            for s in range(len(strings)):
+                strings[s] = strings[s].capitalize()
+            anno = ' '.join(strings)
+        all_anno = glob.glob(annotation_path + '/' + d + '/*')
+        for ann in all_anno:
+            with open(ann, 'w') as anno_file:
+                anno_file.write(anno)
+    print('done')
+
+
 def train_model(model: keras.models.Sequential,
                 image_path: str,
                 annotation_path: str):
@@ -68,9 +115,13 @@ def train_model(model: keras.models.Sequential,
         all_img_path = image_path + '/' + directory + '/*.jpg'
         all_images = glob.glob(all_img_path)
         images.append(all_images)
-        all_annotation_path = annotation_path + '/' + directory + '/*.jpg'
+        all_annotation_path = annotation_path + '/' + directory + '/*'
         all_annotations = glob.glob(all_annotation_path)
         annotations.append(all_annotations)
+
+    # Take random sample of images
+    images = [random.sample(sublist, len(sublist) // 3) for sublist in images]
+
 
     # Open all of the images
     images = flatten(images)
@@ -79,6 +130,7 @@ def train_model(model: keras.models.Sequential,
     # Ram destroyer 9000
     X = np.array(flatten([list(Image.open(i, 'r').getdata()) for i in images]))
     y = np.array(annotations)
+    print(X)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y)
     X_test, X_valid, y_test, y_valid = train_test_split(X_test, y_test)
@@ -123,6 +175,12 @@ if __name__ == '__main__':
     if opt == '--train':
         initialize_model(build_model((400, 500, 3)),
                          './Images', './Annotation')
+
+    if opt == '--fix_images':
+        fix_images('./Images')
+
+    if opt == '--fix_annotations':
+        fix_annotations('./Annotation')
 
     if opt == '--go':
         if len(sys.argv) < 3:
