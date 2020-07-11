4956e4ac99b61552166abaa10488bfd4af8c7a92
Jarred Parr <jparr721@gmail.com>
2020-07-11 15:42:37 -0600

    incremental

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+
+def extract_and_transform_data(data_dir: str):
+    data_transforms = {
+        "train": transforms.Compose(
+            [
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+                ),
+            ]
+        ),
+        "val": transforms.Compose(
+            [
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+                ),
+            ]
+        ),
+    }
+
+    image_datasets = {}
+
+    for breed in os.listdir(data_dir):
+        image_datasets[breed] = datasets.ImageFolder(
+            os.path.join(data_dir, breed)
+        )
+
+    print(image_datasets)
